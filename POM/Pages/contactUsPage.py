79001ecfb6c4a34196e1de606a1e94241d24e2d1
michaal1995 <michaal1995@o2.pl>
2020-11-29 17:29:11 +0100

    sendMsg_test.py -changed name of test: (test_sendMassage_test) into: (test_sendMessage_test_valid_data) -added 3 tests:  test_sendMessage_invalid_email_test  test_sendMessage_unchoosen_subject_heading_test  test_sendMessage_empty_message contactUsPage.py - added new methods:  set_subject_heading_as_customer_service  check_returned_message_about_message_status_valid_data  check_returned_message_about_message_status_invalid_data

--- POM/Pages/contactUsPage.py
+++ POM/Pages/contactUsPage.py
@@ -7,6 +7,10 @@ class ContactUsPage():
         self.order_reference_textbox_by_id = "id_order"
         self.text_messagebox_by_id = "message"
         self.send_button_by_id = "submitMessage"
+        self.subject_heading_box_by_xpath = \
+            "/html/body/div/div[2]/div/div[3]/div/form/fieldset/div[1]/div[1]/div[1]/div"
+        self.message_status_text_by_css_selector = '#center_column > p'
+        self.message_status_error_by_xpath = '//*[@id="center_column"]/div/ol/li'
 
     def enter_email_address(self, email_address):
         self.driver.find_element_by_id(self.email_address_textbox_by_id).clear()
@@ -23,4 +27,20 @@ class ContactUsPage():
     def click_send(self):
         self.driver.find_element_by_id(self.send_button_by_id).click()
 
+    def set_subject_heading_as_customer_service(self):
+        self.driver.find_element_by_xpath(self.subject_heading_box_by_xpath).click()
+        choose = self.driver.find_element_by_xpath(
+            '/html/body/div/div[2]/div/div[3]/div/form/fieldset/div[1]/div[1]/div[1]/div')
+        choose.click()
+        setvalue = self.driver.find_element_by_xpath(
+            '/html/body/div/div[2]/div/div[3]/div/form/fieldset/div[1]/div[1]/div[1]/div/select')
+        setvalue.send_keys('\ue015')
+        setvalue.send_keys('\ue007')
 
+    def check_returned_message_about_message_status_valid_data(self):
+        status_message = self.driver.find_element_by_css_selector(self.message_status_text_by_css_selector).text
+        return status_message
+
+    def check_returned_message_about_message_status_invalid_data(self):
+        status_message = self.driver.find_element_by_xpath(self.message_status_error_by_xpath).text
+        return status_message
--- POM/Tests/sendMsg_test.py
+++ POM/Tests/sendMsg_test.py
@@ -1,8 +1,10 @@
-import unittest
-from selenium import webdriver
 import time
-from POM.Pages.mainPage import mainPage
+import unittest
+
+from selenium import webdriver
+
 from POM.Pages.contactUsPage import ContactUsPage
+from POM.Pages.mainPage import mainPage
 
 
 class SendMsgTest(unittest.TestCase):
@@ -13,7 +15,7 @@ class SendMsgTest(unittest.TestCase):
         cls.driver.maximize_window()
         time.sleep(3)
 
-    def test_sendMassage_test(self):
+    def test_sendMessage_test_valid_data(self):
         driver = self.driver
         driver.get("http://automationpractice.com/")
 
@@ -22,12 +24,68 @@ class SendMsgTest(unittest.TestCase):
         time.sleep(5)
 
         contactsite = ContactUsPage(driver)
+        contactsite.set_subject_heading_as_customer_service()
         contactsite.enter_text_message("jakis text")
         contactsite.enter_order_reference("1234")
         contactsite.enter_email_address("example13@mail.com")
         time.sleep(4)
         contactsite.click_send()
         time.sleep(2)
+        msg_status = contactsite.check_returned_message_about_message_status_valid_data()
+        self.assertEqual(msg_status, "Your message has been successfully sent to our team.")
+
+    def test_sendMessage_invalid_email_test(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/")
+
+        firstsite = mainPage(driver)
+        firstsite.goToContactUs()
+        time.sleep(1)
+
+        contactsite = ContactUsPage(driver)
+        contactsite.enter_text_message("jakis text")
+        contactsite.enter_order_reference("1234")
+        contactsite.enter_email_address("example13mail.com")
+        contactsite.set_subject_heading_as_customer_service()
+        contactsite.click_send()
+        time.sleep(1)
+        msg_status = contactsite.check_returned_message_about_message_status_invalid_data()
+        self.assertEqual(msg_status, "Invalid email address.")
+
+    def test_sendMessage_unchoosen_subject_heading_test(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/")
+
+        firstsite = mainPage(driver)
+        firstsite.goToContactUs()
+        time.sleep(1)
+
+        contactsite = ContactUsPage(driver)
+        contactsite.enter_text_message("jakis text")
+        contactsite.enter_order_reference("1234")
+        contactsite.enter_email_address("example13@mail.com")
+        contactsite.click_send()
+        time.sleep(1)
+        msg_status = contactsite.check_returned_message_about_message_status_invalid_data()
+        self.assertEqual(msg_status, "Please select a subject from the list provided.")
+
+    def test_sendMessage_empty_message(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/")
+
+        firstsite = mainPage(driver)
+        firstsite.goToContactUs()
+        time.sleep(1)
+
+        contactsite = ContactUsPage(driver)
+        contactsite.set_subject_heading_as_customer_service()
+        contactsite.enter_order_reference("1234")
+        contactsite.enter_email_address("example13@mail.com")
+        contactsite.click_send()
+        time.sleep(1)
+        msg_status = contactsite.check_returned_message_about_message_status_invalid_data()
+        self.assertEqual(msg_status, "The message cannot be blank.")
+
 
     @classmethod
     def tearDownClass(cls):
